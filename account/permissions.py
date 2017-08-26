662839054cff37040e4cd064a33a30af23ddd497
duedroid <thossapoldew@gmail.com>
2017-08-27 03:40:18 +0700

    fix authen permission

--- account/permissions.py
+++ account/permissions.py
@@ -4,10 +4,14 @@ from rest_framework.permissions import BasePermission
 class IsVeterinarianAccount(BasePermission):
 
     def has_permission(self, request, view):
-        return (request.user.is_veterinarian and request.user.is_authenticated()) or request.user.is_admin
+        if not request.user.is_authenticated():
+            return False
+        return request.user.is_veterinarian or request.user.is_admin
 
 
 class IsUserAccount(BasePermission):
 
     def has_permission(self, request, view):
-        return (not request.user.is_veterinarian and request.user.is_authenticated()) or request.user.is_admin
+        if not request.user.is_authenticated():
+            return False
+        return not request.user.is_veterinarian or request.user.is_admin
