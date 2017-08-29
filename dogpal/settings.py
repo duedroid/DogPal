7827a94398794685ef969d95252fd48d79791bf6
duedroid <thossapoldew@gmail.com>
2017-08-30 00:50:01 +0700

    fix session expire

--- dogpal/settings.py
+++ dogpal/settings.py
@@ -8,6 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
+SESSION_COOKIE_AGE = 3153600000
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '7f8o4op=!igj#hm$b$t$1h-2^$z9=-po%bdke5%2kavuc83-cn'
 
